b4481509f1004cf5dcb1b42735b3246b86a61442
k4mikoto <i59526@voenmeh.ru>
2025-02-28 19:53:00 +0300

    Input control + random fill

--- main.cpp
+++ main.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
+bool isNumeric(const string& str){
+    for(int a = 0; a < str.size(); a++){
+        if (!isdigit(str[a]))
+           return false;
+    }
+    return true;
+}
+
 int main() {
-    int n;
-    cout << "Введите размер квадратной матрицы: ";
-    cin >> n;
+    string input;
+    int n, temp;
+    bool flag = false;
+    
+    while (!flag) {
+      cout << "Введите размер квадратной матрицы: ";
+      getline(cin,input);
+      if (isNumeric(input)) {
+      n = stoi(input);
+      flag = true;
+      }
+    }
 
     vector<vector<int>> matrix(n, vector<int>(n));
-
-    // Ввод матрицы
-    cout << "Введите элементы матрицы:" << endl;
+    
+    std::random_device rseed;
+    std::mt19937 rng(rseed());
+    std::uniform_int_distribution<int> dist(-100,100);
+        
+    //zapolnenie
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cin >> matrix[i][j];
+            matrix[i][j] = dist(rng);
         }
     }
+    
+    // Вывод матрицы
+    cout << "Изначальная матрица:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 
     // Поиск столбцов без отрицательных элементов и вычисление суммы
     for (int j = 0; j < n; ++j) {
